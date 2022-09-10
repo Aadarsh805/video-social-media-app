4c9615f077e1717fd613be5865ea01f914561014
aadarsh805 <aadarshthakur80501@gmail.com>
2022-09-10 09:40:00 +0530

    video

--- utils/client.ts
+++ utils/client.ts
@@ -1,9 +1,10 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'tp4ln3j8',
+  projectId: '5rwyv487',
   dataset: 'production',
-  apiVersion: '2022-03-10',
+  apiVersion: '2022-09-10',
   useCdn: false,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
+
